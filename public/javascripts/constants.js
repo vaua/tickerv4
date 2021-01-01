8ef798e822e7c157d4b90aefbc4376de546c5f90
Vanja Cvetojevic <vanja@zenterio.com>
2021-01-01 21:24:39 +0100

    Some refurbishing - created senses, actions, constants files and removed some other.

---
+++ public/javascripts/constants.js
@@ -0,0 +1,13 @@
+ // Consts used in triggers
+ const animalSizeBits = 3;
+ const animalTypeBits = 3;
+ const animalShapeBits = 3;
+ const distanceBits = 3;
+ const visionActionBits = 5;
+ const affinityBits = 4;
+ const maxTracts = 64;
+
+ const animalSizeSpace = Math.pow(2, animalSizeBits);
+ const animalTypeSpace = Math.pow(2, animalTypeBits);
+ const animalShapeSpace = Math.pow(2, animalShapeBits);
+ const distanceSpace = Math.pow(2, distanceBits);
